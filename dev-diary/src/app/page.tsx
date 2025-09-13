1d3b48a9d52956351d4f67cb8a05e02050e3ff0b
marcelmarais <marcelmarais2000@gmail.com>
2025-09-13 17:52:42 +0200

    chore: lint

--- dev-diary/src/app/page.tsx
+++ dev-diary/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   }
 
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-black p-8 pb-20 font-sans text-white sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <form
           className="row"
